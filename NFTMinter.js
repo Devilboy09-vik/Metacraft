318aada37ed577b49e27b38d595c247b5fab11a2
Vikram Garg <124725837+Devilboy09-vik@users.noreply.github.com>
2024-06-01 11:32:08 -0400

    Create NFTMinter.js

---
+++ NFTMinter.js
@@ -0,0 +1,55 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+let NFTContainer = [];
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (name , age , dob, place, firstHit,viralHit ,charge) {
+    let NFT = {
+        name: name,
+        age: age,
+        dob: dob,
+        place: place,
+        firstHit :firstHit ,
+        viralHit: viralHit ,
+        charge: charge,
+    }
+    NFTContainer.push(NFT);
+
+}
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+     for (let i = 0; i < NFTContainer.length; i++) {
+        console.log("\n\n_____________________ Singer Details   _________________________\n\n" );
+        console.log("Name: " + NFTContainer[i].name);
+        console.log("Age: " + NFTContainer[i].age);
+        console.log("Dob: " + NFTContainer[i].dob);
+        console.log("Place: " + NFTContainer[i].place);
+        console.log("FirstHit : " + NFTContainer[i].firstHit);
+        console.log("ViralHit : " + NFTContainer[i].viralHit);
+        console.log("Charge: " + NFTContainer[i].charge);
+        console.log("\n\n_____________________	🎵🎵🎵🎵🎵_________________________\n" );
+    }
+}
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+    console.log("Total NFTs  Minted do Far " + NFTContainer.length);
+}
+
+// call your functions below this line
+mintNFT("Arjit Singh",37, "25 April 1987", "India", "Phir Mohabbat", "Tum hi ho", 50000000);
+mintNFT("Justin Bieber",30, "1 March 1994", "USA", "Baby", "One Time",100000000);
+mintNFT("Ellie Goulding",37,"30 December 1986","United Kindom","Starrey Eyed", "First Time",$370,000);
+mintNFT("Jubin Nautiyal",34, "14 June 1989", "India", "EK Mulaqat","EK Mulaqat",250000);
+listNFTs();
+getTotalSupply();
